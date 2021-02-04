c6586add6001c7db14859a672cc77034263b447d
naokivandit <n.i.vandit0924@gmail.com>
2021-02-05 00:02:43 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Country</th>
+          <th>Rate</th>
+          <th>Discription</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(coin) in coins">
+          <td> {{ coin.code }}</td>
+          <td> {{ coin.rate }}</td>
+          <td> {{ coin.description }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+		async asyncData({ $axios }) {
+			const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+			const response = await $axios.$get(url);
+			return {
+				coins: response.bpi
+			};
+		}
+	};
+</script>
+
+<style>
+
+</style>
